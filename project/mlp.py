509ff4690ef759285b7a0de282820cf95addf659
maximepeg <maxime.pegane@gmail.com>
2023-10-11 16:02:59 +0200

    added a gridsearch, attention layers and a visualisation of the categorical embedding (kinda cool ngl)

--- project/mlp.py
+++ project/mlp.py
@@ -79,12 +79,65 @@ class CategMLP(pl.LightningModule):
         categ = self.embedding_layer(categ.float())
         return categ
 
+    def embed_categories(self, data):
+        return self.embedding_layer(data)
+
+
     def common_step(self, batch, batch_idx):
         _, _, y = batch
         y = y.unsqueeze(1).float()
 
         out = self(batch, batch_idx)
         loss = self.loss(out, y)
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        loss = self.common_step(batch, batch_idx)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.common_step(batch, batch_idx)
+        self.log('val_loss', loss)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
+
+
+class AttentionMLP(pl.LightningModule):
+    def __init__(self, input_dim, hidden_layers, output_dim, lr=1e-3, dropout=0.0):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        self.layers.append(nn.Linear(input_dim, hidden_layers[0]))
+        self.attentions = nn.ModuleList()
+        self.dropout = nn.Dropout(dropout)
+
+        for i in range(len(hidden_layers) - 1):
+            self.layers.append(nn.Linear(hidden_layers[i], hidden_layers[i + 1]))
+            self.attentions.append(nn.MultiheadAttention(embed_dim=hidden_layers[i + 1], num_heads=1))
+
+        self.layers.append(nn.Linear(hidden_layers[-1], output_dim))
+        self.loss = nn.MSELoss()
+        self.lr = lr
+
+    def forward(self, x):
+        x = self.layers[0](x)
+        for layer,  attention in zip(self.layers[1:-1], self.attentions):
+            x = torch.relu(layer(x))
+            x = self.dropout(x)
+            x = attention(x, x, x)[0]
+
+        return self.layers[-1](x)
+
+    def common_step(self, batch, batch_idx):
+        x, y = batch
+        y = y.unsqueeze(1).float()
+        x=x.float()
+
+        logits = self(x)
+
+        loss = self.loss(logits, y)
         return loss
 
     def training_step(self, batch, batch_idx):
